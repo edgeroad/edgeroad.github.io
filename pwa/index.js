0926fbdb904044478006cdd98cc10fa799be7701
gianluca.bologna <gianluca.bologna@wolterskluwer.com>
2023-12-14 16:28:42 +0100

    Estratto codice

---
+++ pwa/index.js
@@ -0,0 +1,17 @@
+﻿self.addEventListener("DOMContentLoaded", (event) => { 
+	let iframe = document.getElementById('myiframe');
+	iframe.src = "/index.html"
+})
+
+self.addEventListener("click", function (evnt) {
+	// offre la barra di uscita automaticamente
+	top.location = "https://developer.mozilla.org/en-US/"
+})
+
+var serviceWorkerReg = false;
+self.addEventListener('load', function() {
+	if ('serviceWorker' in navigator) {
+		serviceWorkerReg = navigator.serviceWorker.register('/pwa/service-worker.js');
+	}
+})
+
